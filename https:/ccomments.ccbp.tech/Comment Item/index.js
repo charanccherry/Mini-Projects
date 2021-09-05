0c40299807897e7a3bf22ad98a217cd98cc8c94d
P . Sai Charan Kumar <87646166+charanccherry@users.noreply.github.com>
2021-09-05 21:53:29 +0530

    Create index.js

---
+++ https:/ccomments.ccbp.tech/Comment Item/index.js
@@ -0,0 +1,73 @@
+import {formatDistanceToNow} from 'date-fns'
+
+import './index.css'
+
+const CommentItem = props => {
+  const {commentDetails} = props
+  const {
+    id,
+    username,
+    comment,
+    isLiked,
+    initialClassName,
+    date,
+  } = commentDetails
+  const initial = username ? username[0].toUpperCase() : ''
+  const likeTextClassName = isLiked ? 'button active' : 'button'
+  const likeImageUrl = isLiked
+    ? 'https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png'
+  const onClickLike = () => {
+    const {toggleIsLiked} = props
+    toggleIsLiked(id)
+  }
+  const onDeleteComment = () => {
+    const {deleteComment} = props
+    deleteComment(id)
+  }
+  const postedTime = formatDistanceToNow(date)
+
+  return (
+    <li className="comment-item">
+      <div className="comment-container">
+        <div className={initialClassName}>
+          <p className="initial">{initial}</p>
+        </div>
+        <div>
+          <div className="username-time-container">
+            <p className="username">{username}</p>
+            <p className="time">{postedTime} ago</p>
+          </div>
+          <p className="comment">{comment}</p>
+        </div>
+      </div>
+      <div className="buttons-container">
+        <div className="like-container">
+          <img src={likeImageUrl} alt="like" className="like-image" />
+          <button
+            className={likeTextClassName}
+            type="button"
+            onClick={onClickLike}
+          >
+            Like
+          </button>
+        </div>
+        <button
+          className="button"
+          type="button"
+          onClick={onDeleteComment}
+          testid="delete"
+        >
+          <img
+            className="delete"
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+      <hr className="comment-line" />
+    </li>
+  )
+}
+
+export default CommentItem
