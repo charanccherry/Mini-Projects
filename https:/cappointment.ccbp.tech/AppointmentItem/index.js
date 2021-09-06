a5911671d486b85f81d219700e5635ed20a5b6af
P . Sai Charan Kumar <87646166+charanccherry@users.noreply.github.com>
2021-09-06 23:17:21 +0530

    Create index.js

---
+++ https:/cappointment.ccbp.tech/AppointmentItem/index.js
@@ -0,0 +1,32 @@
+import './index.css'
+
+const AppointmentItem = props => {
+  const {appointmentDetails, toggleIsStarred} = props
+  const {id, date, title, isStarred} = appointmentDetails
+
+  const starImgUrl = isStarred
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
+  const onClickStarred = () => {
+    toggleIsStarred(id)
+  }
+
+  return (
+    <li className="appointment-item">
+      <div className="header-container">
+        <p className="title">{title}</p>
+        <button
+          type="button"
+          testid="star"
+          onClick={onClickStarred}
+          className="star-button"
+        >
+          <img src={starImgUrl} className="star" alt="star" />
+        </button>
+      </div>
+      <p className="date">Date: {date}</p>
+    </li>
+  )
+}
+export default AppointmentItem
