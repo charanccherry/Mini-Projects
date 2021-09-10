e7edf23b6e0c416fa1b5c81b11a635b018f80d93
P . Sai Charan Kumar <87646166+charanccherry@users.noreply.github.com>
2021-09-10 21:11:13 +0530

    Create index.js

---
+++ https:/cminiblog.ccbp.tech/App/Components/Contact/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Contact = () => (
+  <div className="contact-cont">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+      alt="contact"
+      className="contact"
+    />
+    <h1 className="contact-text">Contact</h1>
+  </div>
+)
+export default Contact
