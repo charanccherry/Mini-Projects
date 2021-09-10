cf08faca1f77b7d0a30dbdceeafd94b94dc7e055
P . Sai Charan Kumar <87646166+charanccherry@users.noreply.github.com>
2021-09-10 21:10:43 +0530

    Create index.js

---
+++ https:/cminiblog.ccbp.tech/App/Components/About/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const About = () => (
+  <div className="about-cont">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="about"
+    />
+    <h1 className="about-text">About</h1>
+    <p className="about-desc">I love to create! I am a frontend Developer</p>
+  </div>
+)
+export default About
