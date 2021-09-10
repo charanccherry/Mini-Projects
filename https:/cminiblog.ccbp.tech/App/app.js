374c3949ce3d21ecb0d2b24082bf83c6ad88b3fc
P . Sai Charan Kumar <87646166+charanccherry@users.noreply.github.com>
2021-09-10 21:08:03 +0530

    Create app.js

---
+++ https:/cminiblog.ccbp.tech/App/app.js
@@ -0,0 +1,25 @@
+import {Route, Switch} from 'react-router-dom'
+
+import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <div className="app-cont">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </>
+)
+
+export default App
