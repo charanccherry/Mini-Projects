a4dcbb978d81b0eaab53015f90f20bd83d3de6b2
P . Sai Charan Kumar <87646166+charanccherry@users.noreply.github.com>
2021-09-10 21:12:05 +0530

    Create index.js

---
+++ https:/cminiblog.ccbp.tech/App/Components/Not Found/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const NotFound = () => (
+  <div className="notfound-cont">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="notfound"
+      className="notfound"
+    />
+    <h1 className="notfound-text">Not Found</h1>
+  </div>
+)
+export default NotFound
