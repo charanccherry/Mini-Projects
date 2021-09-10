663e3c277abe484b28d668422390ca487f277af5
P . Sai Charan Kumar <87646166+charanccherry@users.noreply.github.com>
2021-09-10 21:10:03 +0530

    Create index.js

---
+++ https:/cminiblog.ccbp.tech/App/Components/Home/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Home = () => (
+  <div className="home-cont">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+      alt="home"
+      className="home"
+    />
+    <h1 className="home-text">Home</h1>
+  </div>
+)
+export default Home
